756e8ad029d6670c74e96189152463ae737a0fca
nhatdan112 <125143110+nhatdan112@users.noreply.github.com>
2025-05-21 13:51:42 +0700

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,19 @@
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "server.js",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "server.js"
+    }
+  ],
+  "env": {
+    "JWT_SECRET": "@jwt_secret",
+    "MONGO_URI": "@mongo_uri"
+  }
+}
